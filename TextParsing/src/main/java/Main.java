d788fb856f6c2b68da81d110b771d13f474070ae
Lukas Läderach <lukas.laederach@3volutions.ch>
2017-09-25 19:24:03 +0200

    reformat

--- TextParsing/src/main/java/Main.java
+++ TextParsing/src/main/java/Main.java
@@ -1,28 +1,25 @@
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Hashtable;
 
 public class Main {
 
     public static void main(String[] args) {
         Path filePath = Paths.get("C:\\BFH\\Hamlet.txt");
-        HashMap<String,Long> words = new HashMap<>();
+        HashMap<String, Long> words = new HashMap<>();
         try {
             Files.readAllLines(filePath).stream()
                     .map(line -> line.split(" "))
                     .flatMap(Arrays::stream)
                     .map(w -> w.replaceAll("[^a-zA-Z]", "").toLowerCase())
                     .filter(w -> w.length() > 0)
-                    .forEach(w ->{
+                    .forEach(w -> {
                         if (!words.containsKey(w)) {
                             words.put(w, 1l);
-                        }else{
+                        } else {
                             words.put(w, words.get(w) + 1);
                         }
                     });
@@ -30,7 +27,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        words.keySet().stream().forEach(k ->{
+        words.keySet().stream().forEach(k -> {
             System.out.println(k + ":  " + words.get(k));
         });
         Path outputPath = Paths.get("C:\\BFH\\output.csv");
